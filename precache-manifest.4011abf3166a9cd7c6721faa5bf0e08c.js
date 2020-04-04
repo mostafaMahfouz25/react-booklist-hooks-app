a9108dff748eb95a80e57aa7c6f8cb3d98f4e3a4
eraasoft <eraasoft2019@gmail.com>
2020-04-04 19:29:55 +0200

    Updates

---
+++ precache-manifest.4011abf3166a9cd7c6721faa5bf0e08c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61d06c00cc53b2406cb6eba3ea428715",
+    "url": "/react-hooks-app/index.html"
+  },
+  {
+    "revision": "3c56259527ed19c5d1bf",
+    "url": "/react-hooks-app/static/css/main.52c41aa6.chunk.css"
+  },
+  {
+    "revision": "56589a159596cd83e51d",
+    "url": "/react-hooks-app/static/js/2.4ceb94f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-app/static/js/2.4ceb94f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c56259527ed19c5d1bf",
+    "url": "/react-hooks-app/static/js/main.37bc00e2.chunk.js"
+  },
+  {
+    "revision": "66c504ba670bd4a7cc38",
+    "url": "/react-hooks-app/static/js/runtime-main.1dce828c.js"
+  }
+]);
